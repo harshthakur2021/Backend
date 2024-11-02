5fd932b8360e09516958208bf6675ef178280c3e
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2024-11-02 13:00:57 +0530

    Added

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
- const catme= require ('cat-me')
-console.log(catme())
+const express = require('express')
+const app = express()
+const  morgan = require('morgan')
+const port = 3000
+
+ app.use (morgan('dev'))
+
+app.use((req,res,next)=>{
+    console.log("This is middleware")
+})
+
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/About', (req, res) => {
+    res.send('Hello')
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
