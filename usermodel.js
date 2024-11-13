fadab2f7bbca5edf10ec0415b9fc7870737958c0
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2024-11-13 19:50:06 +0530

    Modiflied

--- usermodel.js
+++ usermodel.js
@@ -2,8 +2,12 @@ const mongoose = require ("mongoose");
  mongoose.connect ("mongodb://127.0.0.1:27017/Practicekaro");
 
 
- // Schema matlab aapko ye batana waala har document mein kya kya hoga
+ 
+// Amazon ka database hai usme har ek collection users ka data hai
+// Us user me ek user ki baat ho rhi that is document. 
 
+// Schema matlab aapko ye batana waala har document mein kya kya hoga
+ 
 
   const userschema= mongoose.Schema({
   username: String,
